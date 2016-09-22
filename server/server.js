b285aa75d405faa789e74aaabd49488ead6dfdb2
Ben Mahowald <ben.mahowald@gmail.com>
2016-09-22 16:18:01 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var mongoURI = "mongodb://localhost:27017/assignments";
+var MongoDB = mongoose.connect(mongoURI).connection;
+
+MongoDB.on('error', function (err) {
+    console.log('mongodb connection error:', err);
+});
+
+MongoDB.once('open', function () {
+  console.log('mongodb connection open!');
+});
