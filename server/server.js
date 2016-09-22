d4a2a1fd5dd7a54c75c4afa092b2acfb37cbea85
Ben Mahowald <ben.mahowald@gmail.com>
2016-09-22 17:11:54 -0500

    db save success

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose');
 
 
 var port = process.env.PORT || 4952;
-
+var Grading = require('../models/assignments.js');
 
 var mongoURI = "mongodb://localhost:27017/assignments";
 var MongoDB = mongoose.connect(mongoURI).connection;
@@ -26,6 +26,30 @@ app.listen(port, function () {
   console.log('listeningon on port:', port);
 });
 
+app.get('/testAssignments', function(req, res) {
+  var dummy = new Grading({
+    student_name:
+      {
+        firstName: "dummy",
+        lastName: "head"
+      },
+    assignment_number: 1,
+    score: 100,
+    completed: true,
+    date_completed: new Date()
+  });
+
+  dummy.save(function(err) {
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+    else {
+      console.log('save successfully');
+      res.sendStatus(200);
+    }
+  });
+});
 app.get('/assignments', function(req, res) {
   res.sendStatus(200);
 }); // end get route
